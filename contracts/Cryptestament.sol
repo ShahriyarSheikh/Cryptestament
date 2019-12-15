377fde4e88a077d9be24466eb405c1baa603e729
shahriyarsheikh <m.shahriyarsheikh@gmail.com>
2019-12-15 23:23:01 +0500

    added payable methods

--- contracts/Cryptestament.sol
+++ contracts/Cryptestament.sol
@@ -1,13 +1,14 @@
 pragma solidity >=0.4.22 <0.6.0;
 
-import {DateTimeLibrary} from "././DateTimeLibrary.sol";
+import {DateTimeLibrary} from "./DateTimeLibrary.sol";
 
 contract CryptestamentMainContract {
     
     /* Variables */
-    address _arbitrator;
-    address _owner;
+    address payable _arbitrator;
+    address payable _owner;
     uint256 _fixedCommission;
+    bool _locked;
     
     /* Modifiers */
     modifier isOwner(){
@@ -30,7 +31,7 @@ contract CryptestamentMainContract {
     
     //stored against adress will indicate the initiator
     struct TestamentDetails {
-        address[] nominees;
+        address payable[] nominees;
         uint256 balanceToSend;
         bool canClaim;
         uint256 timeRemaining;
@@ -43,22 +44,28 @@ contract CryptestamentMainContract {
     mapping(address => TestamentDetails) _userTestament;
     
     
-    constructor(address arbitrator, uint256 fixedCommission) public{
+    constructor(address payable arbitrator, uint256 fixedCommission) public{
         _arbitrator = arbitrator;
         _owner = msg.sender;
         _fixedCommission = fixedCommission;
     }
     
     
-    function AddTestament(address[] calldata nominees, uint256 balanceToSend) external {
+    function AddTestament(address payable[] calldata nominees, uint256 timeRemaining, uint256 daysToExtend) external payable {
         TestamentDetails memory testamentDetails;
         testamentDetails.nominees = nominees;
-        testamentDetails.balanceToSend = balanceToSend;
+        testamentDetails.balanceToSend = msg.value;
+        testamentDetails.timeRemaining = timeRemaining;
+        testamentDetails.daysToExtend = daysToExtend;
         _userTestament[msg.sender] = testamentDetails;
     }
     
-    function ShowTestament() external returns (address[] memory,uint256,bool){
-        return (_userTestament[msg.sender].nominees,_userTestament[msg.sender].balanceToSend,_userTestament[msg.sender].canClaim);
+    function ShowTestament() external returns (address payable[] memory,uint256,bool, uint256,uint256){
+        return (_userTestament[msg.sender].nominees,
+        _userTestament[msg.sender].balanceToSend,
+        _userTestament[msg.sender].canClaim,
+        _userTestament[msg.sender].daysToExtend,
+        _userTestament[msg.sender].timeRemaining);
     }
     
     //TODO
@@ -67,9 +74,12 @@ contract CryptestamentMainContract {
     //TODO
     //Extend testament by daysToExtend
     function RefreshTestament() external returns (bool){
-        TestamentDetails memory testamentDetails = _userTestament[msg.sender]; 
+        TestamentDetails storage testamentDetails = _userTestament[msg.sender]; 
         
          if(CheckIfTimeRemains(testamentDetails.timeRemaining)){
+            
+            testamentDetails.timeRemaining += testamentDetails.daysToExtend * 1 days;
+            
             return true;
         }
         return false;
@@ -77,13 +87,20 @@ contract CryptestamentMainContract {
     
     //TODO
     //First check if testament exists, then check if the user who is claming the testament exists in the nominees   
-    function ClaimTestament(address testamentOwner) external returns (bool){
+    function ClaimTestament(address testamentOwner) external payable {
         TestamentDetails memory testamentDetails = _userTestament[testamentOwner];
         
-        if(CheckIfTimeRemains(testamentDetails.timeRemaining)){
-            return true;
+        require(CheckIfTimeRemains(testamentDetails.timeRemaining),"Claim time has not yet occured");
+        uint amount = testamentDetails.balanceToSend - 1000000; //Commission
+        //TODO
+       
+        for(uint256 i=0; i < testamentDetails.nominees.length;i++){
+            lock();
+            uint sendAmount = amount / testamentDetails.nominees.length;
+            testamentDetails.nominees[i].transfer(sendAmount);
+            unlock();
         }
-        return false;
+       
     }
     
     //TODO
@@ -99,6 +116,25 @@ contract CryptestamentMainContract {
         if(now > timeRemaining)
             return true;
         return false;
+    }
+    
+    function lock() internal {
+        if(!_locked){
+            _locked = true;
+        }else{
+            revert("Transaction is locked, reenterency not possible");
+        }
+    }
+    
+    function unlock() internal  {
+        _locked = true;
+    }
+    
+    /* Public Functions */
+    function TimeChecker(uint256 timeRemaining) public returns (bool,uint){
+        if(now > timeRemaining)
+            return (true, now);
+        return (false, now);
     }
     
 
