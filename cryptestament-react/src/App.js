ad85e0750cc34c1e945f6806d538f216b9d6db85
shahriyarsheikh <m.shahriyarsheikh@gmail.com>
2019-12-17 04:49:48 +0500

    button mocs added

--- cryptestament-react/src/App.js
+++ cryptestament-react/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import Web3 from "web3";
 import './App.css';
 import { CRYPTESTAMENT_ABI, CRYPTESTAMENT_ADDRESS } from "./config.js";
-import  Web3Connector  from "./Helpers/Web3Connector.js";
-import { BrowserRouter as Router, Link, NavLink, Redirect, Prompt} from 'react-router-dom';
+import Web3Connector from "./Helpers/Web3Connector.js";
+import { BrowserRouter as Router, Link, NavLink, Redirect, Prompt } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 
 // function App() {
@@ -15,9 +15,9 @@ import Route from 'react-router-dom/Route';
 // }
 
 class App extends Component {
-state = {
-  account: '', userName:'' ,balance:0
-}
+  state = {
+    account: '', userName: '', balance: 0,nominees:'', timeRemaining:0, daysToExtend:0 , valueOfTestament:0
+  }
 
   componentWillMount() {
     //this.loadBlockchainData();
@@ -26,8 +26,11 @@ state = {
 
   constructor(props) {
     super(props)
-    this.state = { account: '', userName:'' ,balance:0}
+    this.state = { account: '', userName: '', balance: 0, nominees:'', timeRemaining:0, daysToExtend:0, valueOfTestament:0 }
     console.log(this.state)
+    
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async loadBlockchainData() {
@@ -40,65 +43,172 @@ state = {
   }
 
 
-  enableTorus = async () =>{
+  enableTorus = async () => {
     try {
       var result = await Web3Connector.initialize();
       const userInfo = await result.getUserInfo();
       console.log(userInfo)
-      
+
       var accounts = await Web3Connector.web3.eth.getAccounts();
       console.log(accounts)
-      var balance = await  Web3Connector.web3.eth.getBalance(accounts[0]);
+      var balance = await Web3Connector.web3.eth.getBalance(accounts[0]);
       console.log(balance)
 
-      this.setState({account:accounts[0]})
-      this.setState({balance:balance})
-      this.setState({userName:userInfo.name})
-    
+      this.setState({ account: accounts[0] })
+      this.setState({ balance: balance })
+      this.setState({ userName: userInfo.name })
+
+      console.log(this.state)
+
     } catch (error) {
       console.error(error)
     }
+  }
+
+  handleSubmitOfTestament(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
 
 
   render() {
     return (
       <Router>
-      <Route path="/" exact render = {
+        <Route path="/" exact render={
           () => {
-            return (   <div className="container">
-            <div className="row align-items-center h-100">
-              <div className="col-6 mx-auto">
-                <div className="h-100 justify-content-center">
-                  <div align="center">
-                    <h1 >Cryptestament</h1>
-                    <p>Dapp for tracking user's testament and proof of death POC</p>
+            return (
+              <div className="container">
+                <div className="row align-items-center h-100">
+                  <div className="col-6 mx-auto">
+                    <div className="h-100 justify-content-center">
+                      <div align="center">
+                        <h1 >Cryptestament</h1>
+                        <p>Dapp for tracking user's testament and proof of death POC</p>
+                      </div>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="row align-items-center h-100">
-              <div className="col-6 mx-auto">
-                <div align="center">
-                  <button className="btn-primary" onClick={this.enableTorus}>Log in using Torus</button>
+                <div className="row align-items-center h-100">
+                  <div className="col-6 mx-auto">
+                    <div align="center">
+                      <button className="btn-primary" onClick={this.enableTorus}>Log in using Torus</button>
+                    </div>
+                  </div>
                 </div>
+
+
+                <label></label>
+
+                <div class="row">
+                  <div class="col">
+                    <div align="left">
+                      Hello : {this.state.userName}
+                      <br />
+                      Balance: {this.state.balance}
+                      <br />
+                      Address: {this.state.account}
+                    </div>
+                  </div>
+                </div>
+
+                <label></label>
+
+                <div class="row">
+                  <div class="col">
+                    <div align="left">
+                      <h3>Owner of Testament Functionalities</h3>
+                      <button className="btn btn-secondary" disabled={this.state.account === '' ? true : false}>Get User Details</button>
+                      <button className="btn btn-secondary" disabled={this.state.account === '' ? true : false}>Refresh Timeline</button>
+                      <button className="btn btn-secondary" disabled={this.state.account === '' ? true : false}>View Current Testament</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col">
+                    <div align="left">
+                      <h5>Submit Testament</h5>
+                    <form onSubmit={this.handleSubmitOfTestament}>
+                      <label>
+                        Amount to give to nominee:
+                        <input type="text" value={this.state.valueOfTestament} onChange={this.handleChange} />
+                      </label>
+                      <br/>
+                      <label>
+                        Time (in unix timestamp):
+                        <input type="number" value={this.state.timeRemaining} onChange={this.handleChange} />
+                      </label>
+                      <br/>
+                      <label>
+                        Days to extend :
+                        <input type="text" value={this.state.daysToExtend} onChange={this.handleChange} />
+                      </label>
+                      <br/>
+                      <label>
+                        Nominees :
+                        <input type="text" value={this.state.nominees} onChange={this.handleChange} />
+                      </label>
+                      <br/>
+                      <input className="btn btn-primary" type="submit" value="Submit" disabled={this.state.account === '' ? true : false}/>
+                    </form>
+                    </div>
+                  </div>
+                </div>
+
+                <label></label>
+
+                <div class="row">
+                  <div class="col">
+                    <div align="left">
+                      <h3>Nominee functionalities</h3>
+                      <button className="btn btn-info" disabled={this.state.account === '' ? true : false}>Claim</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-6">
+                    <form onSubmit={(event) => {
+                      event.preventDefault()
+                      //this.props.createTask(this.task.value)
+                    }}>
+                      <input
+                        id="ClaimDate"
+                        ref={(input) => {
+                          this.task = input
+                        }}
+                        type="text"
+                        className="form-control"
+                        placeholder="Check claim date by entering owner address"
+                        required />
+                      <input type="submit" hidden={this.state.account == '' ? true : false} onClick={this.sampleFunction}/>
+                    </form>
+                  </div>
+                </div>
+                
+
               </div>
-            </div>
-          </div>);
+
+
+
+
+            );
           }}
-      />
-      
-      <Route path="/dashboard" exact render = {
+        />
+
+        <Route path="/dashboard" exact render={
           () => {
-            return (   
-            <div className="container">
-            hello 
-          </div>
-          );
+            return (
+              <div className="container">
+                hello {this.state.userName}
+              </div>
+            );
           }}
-      
-      
-      ></Route>
+
+
+        ></Route>
       </Router>
     );
   }
